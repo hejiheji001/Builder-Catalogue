8c753dc0ecf461ef0858833e1b07fc70b964b1c3
FireAwayH <henry.he@lego.com>
2025-11-19 17:02:06 +0800

    add docker support

--- Builder Catalogue.AppHost/AppHost.cs
+++ Builder Catalogue.AppHost/AppHost.cs
@@ -1,5 +1,17 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<BuilderCatalogue_Api>("api");
+// Add a Docker Compose environment & Enable dashboard with custom configuration
+var compose = builder.AddDockerComposeEnvironment("compose").WithDashboard(dashboard => {
+    dashboard.WithHostPort(8888).WithForwardedHeaders(enabled: true);
+});
+
+// Add your services to the Docker Compose environment
+builder.AddProject<BuilderCatalogue_Api>("apiservice").PublishAsDockerComposeService((resource, service) =>
+{
+    service.Name = "api";
+});
+
 
 builder.Build().Run();
